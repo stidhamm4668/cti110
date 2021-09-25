a783b894f4c228deec542b91b07636d15d49dd8c
stidhamm4668 <91209182+stidhamm4668@users.noreply.github.com>
2021-09-25 11:54:07 -0400

    Add files via upload
    First HW submission

---
+++ P1HW1_Output_StidhamMatthew.py
@@ -0,0 +1,23 @@
+user_num = int(input('Enter integer:\n'))
+
+print('You entered:', user_num)
+
+user_squared= user_num * user_num
+
+print(user_num, 'squared is', user_squared)
+
+user_cubed= user_num * user_num * user_num
+
+print('And', user_num, 'cubed is', user_cubed, '!!')
+
+print('Enter another integer:')
+
+user_num2= int(input())
+
+user_add= user_num + user_num2
+
+user_multi= user_num * user_num2
+
+print(user_num,'+',user_num2,'is',user_add)
+
+print(user_num,'*',user_num2,'is',user_multi)
